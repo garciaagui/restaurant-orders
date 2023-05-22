4366201ba3b0c53155e06248a4aee250ef5880d5
Guilherme Garcia <garciaguig@gmail.com>
2023-05-22 14:55:36 -0300

    test: Add tests for `Ingredient` class

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,24 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import (
+    Ingredient,
+    Restriction,
+)  # noqa: F401, E261, E501
 
 
-# Req 1...
-def test_ingredient():
-    pass
+def test_ingredient(capsys):
+    bacon = Ingredient("bacon")
+    farinha = Ingredient("farinha")
+
+    assert bacon.name == "bacon"
+    assert bacon.restrictions == {
+        Restriction.ANIMAL_MEAT,
+        Restriction.ANIMAL_DERIVED,
+    }
+
+    assert bacon.__hash__() == hash("bacon")
+    assert bacon.__eq__(farinha) is False
+    assert bacon.__eq__(bacon) is True
+
+    print(bacon)
+    out, _ = capsys.readouterr()
+
+    assert out == "Ingredient('bacon')\n"
