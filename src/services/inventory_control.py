8705a1bed0d0b59be8d7b5c263064a6d40ce260f
Guilherme Garcia <garciaguig@gmail.com>
2023-05-23 15:15:09 -0300

    feat: Implements `InventoryMapping` functions

--- src/services/inventory_control.py
+++ src/services/inventory_control.py
@@ -4,6 +4,7 @@ from typing import Dict
 from src.models.dish import Recipe
 from src.models.ingredient import Ingredient
 
+
 BASE_INVENTORY = "data/inventory_base_data.csv"
 
 Inventory = Dict[Ingredient, int]
@@ -20,15 +21,23 @@ def read_csv_inventory(inventory_file_path=BASE_INVENTORY) -> Dict:
     return inventory
 
 
-# Req 5
 class InventoryMapping:
     def __init__(self, inventory_file_path=BASE_INVENTORY) -> None:
         self.inventory = read_csv_inventory(inventory_file_path)
 
-    # Req 5.1
     def check_recipe_availability(self, recipe: Recipe):
-        pass
+        for ingredient, amount in recipe.items():
+            if (
+                ingredient not in self.inventory
+                or amount > self.inventory[ingredient]
+            ):
+                return False
 
-    # Req 5.2
+        return True
+
     def consume_recipe(self, recipe: Recipe) -> None:
-        pass
+        if not self.check_recipe_availability(recipe):
+            raise ValueError("Cannot be consumed, recipe is not available.")
+
+        for ingredient, amount in recipe.items():
+            self.inventory[ingredient] -= amount
